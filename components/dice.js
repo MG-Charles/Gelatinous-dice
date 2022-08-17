0ffa9325de821eff4fdb2b843ba48a6b28915521
ChuckLeon <charles_heon@hotmail.com>
2022-08-16 21:11:35 -0400

    added animations

--- components/dice.js
+++ components/dice.js
@@ -6,6 +6,8 @@ class Dice extends React.Component {
         super(props);
         this.state = {            
             numberRolled: 0,
+            numberArray: Array.from({length: this.props.max}, (_, i) => i + 1),
+            isRolling: false,
         };
     
         // Cette liaison est nécéssaire afin de permettre
@@ -14,11 +16,19 @@ class Dice extends React.Component {
     }
 
     roll(){        
-        let number = Math.floor(Math.random() * this.props.max) + 1;      
+        let number = Math.floor(Math.random() * this.props.max) + 1;              
 
         this.setState(state => ({
-            numberRolled: number
+            numberRolled: 0,
+            isRolling: true,            
         }));
+
+        setTimeout(() => {
+            this.setState(state => ({
+                numberRolled: number,
+                isRolling: false,
+            }));
+        }, this.props.max * 300 + 300);        
     }
 
     render(){
@@ -26,7 +36,12 @@ class Dice extends React.Component {
             <div className={styles.dice} onClick={this.roll}>
                 <div className={styles.content}>
                     <h3 className={styles.text}>Click to roll this d{this.props.max}</h3>
-                    <div className={styles.number}>{this.state.numberRolled}</div>
+                    {/* <div className={styles.number}>{this.state.numberRolled}</div> */}
+                    <div className={`${styles.numberContainer} ${(this.state.isRolling ? styles.rolling : '')}`}>
+                        {this.state.numberArray.map((x, i) =>
+                            <p key={i + 1} className={`${styles.number} ${(this.state.numberRolled == i + 1 ? styles.active : '')}`}>{i + 1}</p>
+                        )}
+                    </div>
                 </div>
             </div>
         );
